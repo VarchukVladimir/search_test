134554b6bbad73b73b3ac83afd03ce503328953a
VarchukVladimir <fildes@gmail.com>
2014-07-17 17:23:59 +0400

    script update

--- sphinx_test/run_test.sh
+++ sphinx_test/run_test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 CUR_DIR=`pwd`
+if [ $# -ne 1 ] 
+then
+    echo "Usage: `basename $0` index path"
+    exit $WRONG_ARGS
+fi
+INDEX_PATH=$1
+
+sed s@{ABS_PATH}@$CUR_DIR@ sphinx.conf.template | \
+sed s@{INDEX_PATH}@$INDEX_PATH@ > sphinx.conf
+
 CONF_FILE=$CUR_DIR/sphinx.conf
 if ! [ -f index ]; then
 	mkdir -p index
 fi
 
-indexer -c $CONF_FILE testindex
+time indexer -c $CONF_FILE testindex
