df92578b6ebdf484ab821f27a5fcd82f9df17282
VarchukVladimir <fildes@gmail.com>
2014-08-04 15:12:15 +0400

    update clucene test script

--- Clucene_test/incremental_test.sh
+++ Clucene_test/incremental_test.sh
@@ -10,6 +10,9 @@ fi
 FILE_COUNTER=0
 DIR_COUNTER=0
 rm -fr "$2"/*
+rm "$4"_time.txt
+rm -fr "$3"/*
+
 for file in $1/*; do
 	if [ $FILE_COUNTER -eq $4 ] 
 	then
@@ -17,9 +20,9 @@ for file in $1/*; do
 		let DIR_COUNTER=DIR_COUNTER+1
 		if [ $DIR_COUNTER -eq 1 ] 
 		then
-			{ time ./build/bin/cl_demo -build "$2" "$3" ; } 2> time.txt
+			{ time ./build/bin/cl_demo -build "$2" "$3" ; } 2> "$4"_time.txt
 		fi 
-		{ time ./build/bin/cl_demo -add "$2" "$3" ; } 2>> time.txt
+		{ time ./build/bin/cl_demo -add "$2" "$3" ; } 2>> "$4"_time.txt
 		rm -fr "$2"/*
 	fi	
 	
