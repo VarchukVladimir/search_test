77f17b75b5e0814745783aedfd7f0e6a7218ef63
VarchukVladimir <fildes@gmail.com>
2014-08-04 15:14:13 +0400

    sphinx_test/run_test_merge.sh

--- sphinx_test/run_test_merge.sh
+++ sphinx_test/run_test_merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CUR_DIR=`pwd`
-if [ $# -ne 1 ] 
+if [ $# -lt 1 ] 
 then
     echo "Usage: `basename $0` index path"
     exit $WRONG_ARGS
@@ -11,12 +11,13 @@ INDEX_PATH=$1
 sed s@{ABS_PATH}@$CUR_DIR@ sphinx.conf.template | \
 sed s@{INDEX_PATH}@$INDEX_PATH@ > sphinx.conf
 
+rm "$2"_time.txt
 CONF_FILE=$CUR_DIR/sphinx.conf
 if ! [ -f index ]; then
 	mkdir -p index
 fi
 
-{ time indexer -c $CONF_FILE testindex_delta ; } 2>> time.txt
+{ time indexer -c $CONF_FILE testindex_delta ; } 2>> "$2"_time.txt
 
-{ time indexer -c $CONF_FILE --merge testindex testindex_delta ; } 2>> time.txt
+{ time indexer -c $CONF_FILE --merge testindex testindex_delta ; } 2>> "$2"_time.txt
 
